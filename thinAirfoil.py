6cfc93e9e0b53f49408c99e60100f4cef1fd2df0
cibinjoseph <cibinjoseph92@gmail.com>
2020-09-02 18:34:44 +0530

    Split xvec and compute coords to make it faster

--- thinAirfoil.py
+++ thinAirfoil.py
@@ -7,6 +7,12 @@ import sys
 _nPoints = 5001
 _eps = np.finfo(np.pi).eps
 
+def checkAirfoilType(airfoilCode):
+    if len(airfoilCode) == 4:
+        return 'NACA4'
+    else:
+        return None
+
 def getNACA4(mpxx, n):
     """ Returns NACA 4-digit airfoil camberline coordinates and slope """
     m = int(mpxx[0])/100.0
@@ -14,20 +20,18 @@ def getNACA4(mpxx, n):
 
     thetavec = np.linspace(0.0, np.pi, n)
     xvec = (1-np.cos(thetavec))/2.0
-    yvec = []
-    dyvec = []
-    for x in xvec:
-        if (x < p):
-            y = (2.0*p*x - x*x)/(p*p)
-            dy = (2.0*p - 2.0*x)/(p*p)
-        else:
-            y = (1.0 - 2.0*p + 2.0*p*x - x*x)/((1 - p)**2.0)
-            dy = (2.0*p - 2.0*x)/((1 - p)**2.0)
-        yvec.append(y)
-        dyvec.append(dy)
 
-    yvec = np.array(yvec)*m
-    dyvec = np.array(dyvec)*m
+    # Split xvec using position of max camber
+    xFore = xvec[xvec < p]
+    yFore = (2.0*p*xFore - xFore**2.0)/(p*p)
+    dyFore = (2.0*p - 2.0*xFore)/(p*p)
+
+    xRear = xvec[xvec >= p]
+    yRear = (1.0 - 2.0*p + 2.0*p*xRear - xRear**2.0)/((1 - p)**2.0)
+    dyRear = (2.0*p - 2.0*xRear)/((1 - p)**2.0)
+
+    yvec = np.concatenate((yFore, yRear))*m
+    dyvec = np.concatenate((dyFore, dyRear))*m
 
     return thetavec, xvec, yvec, dyvec
 
@@ -44,9 +48,10 @@ def getCoeffs(mpxx, alphaDeg):
     """ Calculates CL and CM at alphaDeg for NACA 4-digit airfoil """
     th, x, y, dy = getNACA4(mpxx, _nPoints)
     A0, A1, A2 = calcFourierCoeffs(alphaDeg, th, dy)
+
     CL = 2.0*np.pi*(A0+A1*0.5)
     if np.abs(CL) < _eps:
-        xcp = None
+        xcp = 0.25
     else:
         xcp = 0.25-(np.pi*0.25*(A1-A2))/CL
 
@@ -72,28 +77,32 @@ def main():
         if args[1] == '-h' or args[1] == '--help':
             usage()
         else:
-            mpxx = args[1]
-            # Print lift curve characteristics for airfoil
-            CL0 = getCoeffs(mpxx, 0.0)['CL']
-            CL2 = getCoeffs(mpxx, 2.0)['CL']
-            CLa = (CL2-CL0)/(2.0*np.pi/180.0)
-            alpha0 = -CL0/CLa*(180.0/np.pi)
+            airfoilCode = args[1]
+            if checkAirfoilType(airfoilCode) == 'NACA4':
+                mpxx = airfoilCode
+                # Print lift curve characteristics for airfoil
+                CL0 = getCoeffs(mpxx, 0.0)['CL']
+                CL2 = getCoeffs(mpxx, 2.0)['CL']
+                CLa = (CL2-CL0)/(2.0*np.pi/180.0)
+                alpha0 = -CL0/CLa*(180.0/np.pi)
 
-            print('CLa  (1/rad) = ' + '{: f}'.format(CLa))
-            print('CL0          = ' + '{: f}'.format(CL0))
-            print('alf0 (deg)   = ' + '{: f}'.format(alpha0))
+                print('CLa  (1/rad) = ' + '{: f}'.format(CLa))
+                print('CL0          = ' + '{: f}'.format(CL0))
+                print('alf0 (deg)   = ' + '{: f}'.format(alpha0))
 
     else:
-        mpxx = args[1]
-        print(' Alpha       CL         CM_le      CM_c/4     xcp/c' )
-        for alfStr in args[2:]:
-            alphaDeg = float(alfStr)
-            coeffs = getCoeffs(mpxx, alphaDeg)
-            print('{: f}'.format(alphaDeg), end='  ')
-            print('{: f}'.format(coeffs['CL']), end='  ')
-            print('{: f}'.format(coeffs['CM_le']), end='  ')
-            print('{: f}'.format(coeffs['CM_c/4']), end='  ')
-            print('{: f}'.format(coeffs['xcp']))
+        airfoilCode = args[1]
+        if checkAirfoilType(airfoilCode) == 'NACA4':
+            mpxx = airfoilCode
+            print(' Alpha       CL         CM_le      CM_c/4     xcp/c' )
+            for alfStr in args[2:]:
+                alphaDeg = float(alfStr)
+                coeffs = getCoeffs(mpxx, alphaDeg)
+                print('{: f}'.format(alphaDeg), end='  ')
+                print('{: f}'.format(coeffs['CL']), end='  ')
+                print('{: f}'.format(coeffs['CM_le']), end='  ')
+                print('{: f}'.format(coeffs['CM_c/4']), end='  ')
+                print('{: f}'.format(coeffs['xcp']))
 
 
 if __name__ == '__main__':
